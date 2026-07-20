0843a8d1dd4f3209d4988ee5f7552653f103bf09
imhogyun <hoyashi95@gmail.com>
2026-07-21 01:49:15 +0900

    chore(parallel): PWA sw shell always-learn

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='daedalus-v1';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./style.css']).catch(()=>{})).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
